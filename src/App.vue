419d799427c40ac77132afc8faa217304e6a1af4
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-06-19 16:24:41 -0500

    webcam

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <img alt="Vue logo" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjrqOSD0k9Pq9gIbfG0oKpjZ_DVHFvQG3W0WriClYr_4Etj5LKZJwggUESqbAA8q5LDg7i2PeEr2mTZ2D7GhqqLndTckIkf4jy1O3-lB2yciHaO938CSswLJZAvldYFLCutVHYO89GSld7ngbBbItFLnWSIeJJk4MxiUi0Xt5d8wM5oreDK_6q-LXbCWA/s320/zyro-image.png">
+  <!-- <img alt="Vue logo" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjrqOSD0k9Pq9gIbfG0oKpjZ_DVHFvQG3W0WriClYr_4Etj5LKZJwggUESqbAA8q5LDg7i2PeEr2mTZ2D7GhqqLndTckIkf4jy1O3-lB2yciHaO938CSswLJZAvldYFLCutVHYO89GSld7ngbBbItFLnWSIeJJk4MxiUi0Xt5d8wM5oreDK_6q-LXbCWA/s320/zyro-image.png"> -->
   <CmpGrupoUsuarios/>
-  <CmpMenu/>
   <CmpProfesores/>
+  <CmpCamara/>
 </template>
 
 <script>
 
-import CmpCamara from './components/CmpCamara.vue'
-import CmpMenu from './components/CmpMenu.vue'
+//import CmpCamara from './components/CmpCamara.vue'
+//import CmpMenu from './components/CmpMenu.vue'
 
 import CmpGrupoUsuarios from './components/CmpGrupoUsuarios.vue'
 import CmpProfesores from './components/CmpProfesores.vue'
+import CmpCamara from './components/CmpCamara.vue'
 
 export default {
   name: 'App',
   components: {
     CmpGrupoUsuarios,
     CmpCamara,
-    CmpMenu,
+    //CmpMenu,
     CmpProfesores
   }
 }
--- src/components/CmpCamara.vue
+++ src/components/CmpCamara.vue
@@ -1,63 +1,55 @@
     <template>
         <div>
-        <h1>Componente Cámara</h1>
-        <div>
-            <video ref="video" width="640" height="480"></video>
-            <button @click="takePhoto" :disabled="!isCameraReady">Tomar foto</button>
-        </div>
-        <div>
-            <canvas ref="canvas" width="640" height="480"></canvas>
-            <img v-if="capturedPhoto" :src="capturedPhoto" alt="Captured Photo" />
-        </div>
+        <h1>ASISTENCIA CON CÁMARA</h1>
+        <Button label="Abrir cámara" icon="pi pi-camera" @click="openDialog" />
+        <Dialog :visible="dialogVisible" @hide="hideDialog">
+            <div class="dialog-content">
+            <Webcam ref="webcam" :width="videoWidth" :height="videoHeight" :mirror="false" />
+            <Button label="Tomar foto" @click="takePhoto" />
+            </div>
+            <InputText v-model="dni" placeholder="Ingrese el DNI" />
+        </Dialog>
         </div>
     </template>
     
     <script>
+    import { Button, Dialog, InputText } from 'primevue/button';
+    import { Webcam } from 'vue-web-cam';
+    
     export default {
+        components: {
+        Button,
+        Dialog,
+        InputText,
+        Webcam,
+        },
         data() {
         return {
-            videoStream: null,
-            isCameraReady: false,
-            capturedPhoto: null
+            dialogVisible: false,
+            dni: null,
+            videoWidth: 640,
+            videoHeight: 480,
         };
         },
-        mounted() {
-        this.accessCamera();
-        },
         methods: {
-        accessCamera() {
-            navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then((stream) => {
-                this.videoStream = stream;
-                this.$refs.video.srcObject = stream;
-                this.$refs.video.play();
-                this.isCameraReady = true;
-            })
-            .catch((error) => {
-                console.log('Error accessing camera:', error);
-            });
+        openDialog() {
+            this.dialogVisible = true;
+        },
+        hideDialog() {
+            this.dialogVisible = false;
         },
         takePhoto() {
-            const video = this.$refs.video;
-            const canvas = this.$refs.canvas;
-            const context = canvas.getContext('2d');
-    
-            // Dibujar el video en el canvas
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    
-            // Obtener la imagen en formato base64
-            this.capturedPhoto = canvas.toDataURL('image/png');
-    
-            // Detener el stream de la cámara
-            const tracks = this.videoStream.getTracks();
-            tracks.forEach((track) => track.stop());
-    
-            // Reiniciar la variable del stream y desactivar la cámara
-            this.videoStream = null;
-            this.isCameraReady = false;
-        }
-        }
+            const photoDataUrl = this.$refs.webcam.capture();
+            // Hacer algo con la foto, por ejemplo enviarla a un servidor
+        },
+        },
     };
     </script>
     
+    <style scoped>
+    .dialog-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    </style>  
