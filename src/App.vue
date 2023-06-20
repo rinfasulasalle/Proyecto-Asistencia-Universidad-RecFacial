e9cfd8d550898a8687534ff655eef2d893e6c15a
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-06-20 14:38:21 -0500

    Intento arreglar el componente de la CmpCamara

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <img alt="Vue logo" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjrqOSD0k9Pq9gIbfG0oKpjZ_DVHFvQG3W0WriClYr_4Etj5LKZJwggUESqbAA8q5LDg7i2PeEr2mTZ2D7GhqqLndTckIkf4jy1O3-lB2yciHaO938CSswLJZAvldYFLCutVHYO89GSld7ngbBbItFLnWSIeJJk4MxiUi0Xt5d8wM5oreDK_6q-LXbCWA/s320/zyro-image.png"> -->
   <CmpGrupoUsuarios/>
-  <CmpProfesores/>
+  <!-- <CmpProfesores/> -->
   <CmpCamara/>
 </template>
 
@@ -11,16 +11,16 @@
 //import CmpMenu from './components/CmpMenu.vue'
 
 import CmpGrupoUsuarios from './components/CmpGrupoUsuarios.vue'
-import CmpProfesores from './components/CmpProfesores.vue'
+//import CmpProfesores from './components/CmpProfesores.vue'
 import CmpCamara from './components/CmpCamara.vue'
 
 export default {
   name: 'App',
   components: {
-    CmpGrupoUsuarios,
-    CmpCamara,
+    //CmpGrupoUsuarios,
+    CmpCamara
     //CmpMenu,
-    CmpProfesores
+    //CmpProfesores
   }
 }
 </script>
--- src/components/CmpCamara.vue
+++ src/components/CmpCamara.vue
@@ -1,55 +1,111 @@
-    <template>
+<template>
+    <div class="componente">
+        <h2 class="difuminado">Registros de Asistencia</h2>
         <div>
-        <h1>ASISTENCIA CON CÁMARA</h1>
-        <Button label="Abrir cámara" icon="pi pi-camera" @click="openDialog" />
-        <Dialog :visible="dialogVisible" @hide="hideDialog">
-            <div class="dialog-content">
-            <Webcam ref="webcam" :width="videoWidth" :height="videoHeight" :mirror="false" />
-            <Button label="Tomar foto" @click="takePhoto" />
-            </div>
-            <InputText v-model="dni" placeholder="Ingrese el DNI" />
-        </Dialog>
+            <video ref="videoElement" width="640" height="480"></video>
         </div>
-    </template>
+        <div>
+            <Button @click="takePhoto">
+            <i class="pi pi-camera"></i> Tomar foto
+            </Button>
+        </div>
+        <div v-if="photoTaken">
+            <img :src="photoUrl" alt="Foto de asistencia" />
+            <InputText v-model.trim="dni" placeholder="Ingrese su DNI" />
+            <p>Hora actual: {{ getCurrentTime() }}</p>
+            <Button @click="submitForm" label="Enviar" />
+        </div>
+        <Dialog v-if="showDialog" header="Asistencia Registrada" visible>
+            ¡La asistencia se tomó correctamente!
+        </Dialog>
+    </div>
+</template>
     
-    <script>
-    import { Button, Dialog, InputText } from 'primevue/button';
-    import { Webcam } from 'vue-web-cam';
+<script>
+    import { ref } from 'vue';
+    import { Dialog, Button, InputText } from 'primevue/dialog';
+    import { Toast } from 'primevue/toast';
     
     export default {
         components: {
-        Button,
         Dialog,
+        Button,
         InputText,
-        Webcam,
+        Toast,
         },
         data() {
         return {
-            dialogVisible: false,
-            dni: null,
-            videoWidth: 640,
-            videoHeight: 480,
+            videoElement: null,
+            photoTaken: false,
+            photoUrl: '',
+            dni: '',
+            showDialog: false,
         };
         },
-        methods: {
-        openDialog() {
-            this.dialogVisible = true;
+        mounted() {
+        this.videoElement = this.$refs.videoElement;
+        this.initializeCamera();
         },
-        hideDialog() {
-            this.dialogVisible = false;
+        methods: {
+        initializeCamera() {
+            navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            this.videoElement.srcObject = stream;
+            this.videoElement.play();
+            });
         },
         takePhoto() {
-            const photoDataUrl = this.$refs.webcam.capture();
-            // Hacer algo con la foto, por ejemplo enviarla a un servidor
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.width = this.videoElement.videoWidth;
+            canvas.height = this.videoElement.videoHeight;
+            context.drawImage(
+            this.videoElement,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+            );
+            this.photoUrl = canvas.toDataURL('image/png');
+            this.photoTaken = true;
+        },
+        getCurrentTime() {
+            const currentTime = new Date();
+            return currentTime.toLocaleTimeString();
+        },
+        submitForm() {
+            if (this.dni.length > 0 && this.dni.length <= 8) {
+            this.showDialog = true;
+            } else {
+            this.$toast.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Ingrese un DNI válido (máximo 8 cifras)',
+                life: 3000,
+            });
+            }
         },
         },
     };
-    </script>
+</script>
     
-    <style scoped>
-    .dialog-content {
+<style>
+    .componente {
+        background-image: url("https://s1.1zoom.me/big3/267/336028-SoLoVuShKa.jpg");
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
     }
-    </style>  
+    .difuminado {
+        display: inline-block;
+        background: rgba(192, 23, 214, 0.3);
+        padding: 10px 10px;
+        border-radius: 15px;
+        backdrop-filter: blur(5px);
+        -webkit-backdrop-filter: blur(8px);
+        margin: 20px;
+    }
+</style>
